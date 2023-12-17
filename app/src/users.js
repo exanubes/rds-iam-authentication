68f0cf852ba205d7758d0e46caf1795ee96e1346
Exanubes <blog@exanubes.com>
2023-12-17 15:20:07 +0100

    Create user model

--- app/src/users.js
+++ app/src/users.js
@@ -1,12 +1,23 @@
 const { faker } = require("@faker-js/faker");
+const { pgEnum, pgTable, uuid, varchar, text } = require("drizzle-orm/pg-core");
 
-const UserType = ["admin", "premium", "free"];
+const UserType = pgEnum("UserType", ["admin", "premium", "free"]);
 
+const Users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  fullName: text("full_name").notNull(),
+  phone: varchar("phone", { length: 12 }).unique().notNull(),
+  type: UserType("type").notNull(),
+});
+
+/**
+ * @returns {typeof Users.$inferInsert}
+ * */
 function createNewUser() {
   return {
     fullName: faker.person.fullName(),
     phone: "+" + faker.helpers.fromRegExp(/[0-9]{11}/),
-    type: UserType[
+    type: UserType.enumValues[
       faker.number.int({
         min: 0,
         max: 2,
